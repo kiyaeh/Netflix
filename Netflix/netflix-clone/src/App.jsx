7426d448973222c846aeb7a10cb94e9dc756e53f
kiyaeh <kiyaehcs@gmail.com>
2024-01-29 02:30:31 +0300

    first Commit

---
+++ Netflix/netflix-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
